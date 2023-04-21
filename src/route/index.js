b41fd865e793028d28a8c862a1db7247ab891c74
PopeyeTS <papin.anton@gmail.com>
2023-04-21 13:29:39 +0300

    add page index

--- src/route/index.js
+++ src/route/index.js
@@ -34,11 +34,158 @@ var footer = {
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/', function (req, res) {
+router.get('/index', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {})
+  res.render('index', {
+    layout: 'index',
+    page: [
+      {
+        name: '"Summary"',
+        link: '/summary',
+        title: 'Практика HTML.Резюме. 1 частина',
+        description:
+          'Завдання No1. Завдання No2. Завдання No3.',
+      },
+      {
+        name: '"skills"',
+        link: '/skills',
+        title: 'Практика HTML.Резюме. 2 частина',
+        description:
+          'Завдання No1. Завдання No2. Завдання No3.',
+      },
+      {
+        name: '"education"',
+        link: '/edu',
+        title: 'Практика HTML.Резюме. 3 частина',
+        description:
+          'Завдання No1. Завдання No2. Завдання No3.Завдання No4.Завдання No5.',
+      },
+      {
+        name: '"work"',
+        link: '/work',
+        title: 'Практика HTML.Резюме. 4 частина',
+        description:
+          'Завдання No1. Завдання No2. Завдання No3.Завдання No4.Завдання No5.',
+      },
+      {
+        name: '"person"',
+        link: '/person',
+        title: 'Практика HTML.Резюме. 5 частина',
+        description: 'Завдання No1.',
+      },
+      {
+        name: '"bio"',
+        link: '/bio',
+        title: 'Практика HTML.Резюме. 4 частина',
+        description: 'Завдання No2.',
+      },
+      {
+        name: '"program"',
+        link: '/program',
+        title: 'Практика HTML.Резюме. 6 частина',
+        description: 'Завдання No1.',
+      },
+      {
+        name: '"Web"',
+        link: '/web',
+        title: 'Практика HTML.Резюме. 6 частина',
+        description: 'Завдання No2.',
+      },
+      {
+        name: '"car"',
+        link: '/car',
+        title: 'Знайомство з Bootstrap',
+        description: 'Завдання No1.',
+      },
+      {
+        name: '"mac"',
+        link: '/mac',
+        title: 'Знайомство з Bootstrap',
+        description: 'Завдання No2.',
+      },
+      {
+        name: '"js"',
+        link: '/js',
+        title: 'Знайомство з Bootstrap',
+        description: 'Завдання No3.',
+      },
+      {
+        name: '"facebook"',
+        link: '/facebook',
+        title: 'Знайомство з Bootstrap',
+        description: 'Завдання No4.',
+      },
+      {
+        name: '"task21"',
+        link: '/task21',
+        title: 'Bootstrap: Текст, кнопки та блоки',
+        description: 'Завдання No2.1',
+      },
+      {
+        name: '"task22"',
+        link: '/task22',
+        title: 'Bootstrap: Текст, кнопки та блоки',
+        description: 'Завдання No2.2',
+      },
+      {
+        name: '"task31"',
+        link: '/task31',
+        title: 'Bootstrap: Текст, кнопки та блоки',
+        description: 'Завдання No3.1',
+      },
+      {
+        name: '"shophome"',
+        link: '/shophome',
+        title:
+          'Bootstrap: Навігація, значки та повідомлення',
+        description: 'Завдання No4.1',
+      },
+      {
+        name: '"shopproduct"',
+        link: '/shopproduct',
+        title: 'Bootstrap: Порядковий номер',
+        description: 'Завдання No4.2',
+      },
+      {
+        name: '"shopnews"',
+        link: '/shopnews',
+        title: 'Bootstrap: Порядковий номер',
+        description: 'Завдання No4.3',
+      },
+      {
+        name: '"shoporder"',
+        link: '/shoporder',
+        title: 'Bootstrap: Компоненти форми',
+        description: 'Завдання No5.1',
+      },
+      {
+        name: '"shopcart"',
+        link: '/shopcart',
+        title: 'Bootstrap: Іконки',
+        description: 'Завдання No6.1',
+      },
+      {
+        name: '"shopprofile"',
+        link: '/shopprofile',
+        title: 'Bootstrap: Іконки',
+        description: 'Завдання No6.2',
+      },
+      {
+        name: '"shopreview"',
+        link: '/shopreview',
+        title: 'Знайомство з СSS',
+        description: 'Завдання No7.1',
+      },
+      {
+        name: '"shopcatalog"',
+        link: '/shopcatalog',
+        title: 'Знайомство з СSS',
+        description: 'Завдання No7.2',
+      },
+    ],
+  })
   //                  ↑↑ сюди вводимо JSON дані
 })
 
@@ -48,6 +195,7 @@ router.get('/', function (req, res) {
 router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
+    layout: 'basic',
     // ↙ сюди вводимо JSON дані
     page: {
       title: 'Resume  Summary',
@@ -3419,6 +3567,298 @@ router.get('/shopcatalog', function (req, res) {
         ],
       },
     ],
+    service: {
+      title: 'Our Services',
+      description:
+        'We offer a variety of services to meet your needs, including web design, content creation, and social media management.',
+      buttons: [
+        {
+          text: 'Show More',
+          link: 'https://www.youtube.com/',
+        },
+      ],
+    },
+
+    footer: [
+      [
+        {
+          text: 'Home',
+          link: 'https://www.youtube.com/',
+        },
+        {
+          text: 'Trending',
+          link: 'https://www.youtube.com/feed/trending',
+        },
+        {
+          text: 'Subscriptions',
+          link: 'https://www.youtube.com/feed/subscriptions',
+        },
+        {
+          text: 'Library',
+          link: 'https://www.youtube.com/feed/library',
+        },
+      ],
+      [
+        {
+          text: 'History',
+          link: 'https://www.youtube.com/feed/history',
+        },
+        {
+          text: 'Your Videos',
+          link: 'https://www.youtube.com/feed/my_videos',
+        },
+        {
+          text: 'Live',
+          link: 'https://www.youtube.com/live',
+        },
+        {
+          text: 'Settings',
+          link: 'https://www.youtube.com/account',
+        },
+      ],
+      [
+        {
+          text: 'Watch Later',
+          link: 'https://www.youtube.com/playlist?list=WL',
+        },
+        {
+          text: 'Liked Videos',
+          link: 'https://www.youtube.com/playlist?list=LL',
+        },
+        {
+          text: 'Music',
+          link: 'https://www.youtube.com/music',
+        },
+        {
+          text: 'Gaming',
+          link: 'https://www.youtube.com/gaming',
+        },
+      ],
+      [
+        {
+          text: 'Sports',
+          link: 'https://www.youtube.com/channel/UCo_q6aOlvPH7M-j_XGWVgXg',
+        },
+        {
+          text: 'News',
+          link: 'https://www.youtube.com/news',
+        },
+        {
+          text: 'Fashion & Beauty',
+          link: 'https://www.youtube.com/channel/UC1x8rV_f-2yPpzlN0JWZXIQ',
+        },
+        {
+          text: 'Learning',
+          link: 'https://www.youtube.com/learning',
+        },
+      ],
+      [
+        {
+          text: 'Report History',
+          link: 'https://www.youtube.com/feed/history/report_history',
+        },
+        {
+          text: 'Help',
+          link: 'https://support.google.com/youtube/?hl=en',
+        },
+        {
+          text: 'Send Feedback',
+          link: 'https://support.google.com/youtube/answer/4347644?hl=en',
+        },
+        {
+          text: 'About',
+          link: 'https://www.youtube.com/about/',
+        },
+      ],
+    ],
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/shophome', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('shophome', {
+    layout: 'shop',
+    navigation: {
+      links: [
+        {
+          text: 'Home',
+          href: 'https://github.com/',
+        },
+        {
+          text: 'Contacts',
+          href: 'https://www.google.com/',
+        },
+        {
+          text: 'Help',
+          href: 'https://www.youtube.com/',
+        },
+      ],
+    },
+
+    newsBlock: {
+      title: 'Latest News',
+      cards: [
+        {
+          title: 'COVID-19 Vaccinations Begin in Europe',
+          description:
+            'As the world continues to grapple with the ongoing COVID-19 pandemic, Europe begins its vaccination rollout, with healthcare workers and the elderly among the first to receive the vaccine.',
+          isTop: false,
+          isNew: false,
+        },
+        {
+          title: 'Tesla Stock Surges After Record Sales',
+          description:
+            "Tesla's stock price jumps 10% following the company's announcement of record sales in the last quarter, thanks in part to strong demand for its electric vehicles.",
+          isTop: true,
+          isNew: true,
+        },
+        {
+          title:
+            'New Study Shows Benefits of Meditation for Mental Health',
+          description:
+            'A new study published in the Journal of the American Medical Association finds that practicing mindfulness meditation can lead to improved mental health outcomes for people struggling with depression and anxiety.',
+          isTop: false,
+          isNew: false,
+        },
+        {
+          title: 'NASA Launches New Mars Rover',
+          description:
+            "NASA's Perseverance rover successfully launches on a mission to explore Mars, with the goal of collecting samples of the planet's surface and searching for signs of ancient microbial life.",
+          isTop: false,
+          isNew: true,
+        },
+        {
+          title:
+            'GameStop Shares Soar as Reddit Traders Rally',
+          description:
+            'Shares of GameStop surge as amateur traders on the social media platform Reddit rally to drive up the price, in what some are calling a battle between Wall Street and Main Street.',
+          isTop: false,
+          isNew: false,
+        },
+        {
+          title:
+            'UK Announces Plan to Ban Sale of Gas-Powered Cars by 2030',
+          description:
+            'In an effort to combat climate change, the UK government announces a plan to ban the sale of new gas-powered cars and vans by 2030, with hybrid vehicles to follow in 2035.',
+          isTop: true,
+          isNew: false,
+        },
+        {
+          title:
+            'New Study Shows Link Between Exercise and Longevity',
+          description:
+            'A new study published in the Journal of the American Medical Association suggests that regular exercise can help people live longer, with participants who exercised regularly having a lower risk of premature death.',
+          isTop: false,
+          isNew: false,
+        },
+        {
+          title: 'Amazon Expands Grocery Delivery Service',
+          description:
+            'Amazon announces an expansion of its grocery delivery service, with plans to offer free delivery to Prime members on orders over $35 and to expand its selection of fresh and organic produce.',
+          isTop: false,
+          isNew: true,
+        },
+      ],
+    },
+
+    goodsBlock: {
+      tabs: [
+        {
+          isActive: true,
+          isDisabled: false,
+          text: 'Electronics',
+        },
+        {
+          isActive: false,
+          isDisabled: false,
+          text: 'Home & Kitchen',
+        },
+        {
+          isActive: false,
+          isDisabled: false,
+          text: 'Clothing & Accessories',
+        },
+        {
+          isActive: false,
+          isDisabled: true,
+          text: 'Toys & Games',
+        },
+      ],
+      cards: [
+        {
+          image: 'https://picsum.photos/400/200',
+          title: 'iPhone 13',
+          description:
+            'The latest iPhone model features a new A15 Bionic chip, improved camera system, and longer battery life.',
+          isHot: false,
+          isNew: false,
+        },
+        {
+          image: 'https://picsum.photos/400/200',
+          title: 'MacBook Pro',
+          description:
+            "Apple's high-end laptop features a 16-inch Retina display, powerful M1 Pro or M1 Max chip, and up to 64GB of RAM.",
+          isHot: true,
+          isNew: false,
+        },
+        {
+          image: 'https://picsum.photos/400/200',
+          title: 'AirPods Pro',
+          description:
+            "Apple's premium wireless earbuds feature active noise cancellation, a customizable fit, and up to 4.5 hours of listening time.",
+          isHot: false,
+          isNew: false,
+        },
+        {
+          image: 'https://picsum.photos/400/200',
+          title: 'Sony Bravia XR A90J',
+          description:
+            'This OLED TV boasts a 4K resolution, HDR support, and a high refresh rate for smooth motion handling.',
+          isHot: false,
+          isNew: true,
+        },
+        {
+          image: 'https://picsum.photos/400/200',
+          title: 'Nintendo Switch OLED Model',
+          description:
+            'The latest iteration of the popular gaming console features a larger OLED screen and improved audio quality.',
+          isHot: false,
+          isNew: false,
+        },
+        {
+          image: 'https://picsum.photos/400/200',
+          title: 'Bose SoundLink Revolve+',
+          description:
+            'This portable Bluetooth speaker features 360-degree sound, water-resistant construction, and up to 16 hours of battery life.',
+          isHot: true,
+          isNew: true,
+        },
+      ],
+    },
+    subscribe: {
+      header: 'Unlock Premium Content',
+      description:
+        'Subscribe to access exclusive content and features.',
+      buttons: [
+        {
+          text: 'Register Now',
+          link: 'https://www.youtube.com/',
+        },
+        {
+          text: 'Buy Subscription',
+          link: 'https://www.youtube.com/',
+        },
+      ],
+    },
     service: {
       title: 'Our Services',
       description:
